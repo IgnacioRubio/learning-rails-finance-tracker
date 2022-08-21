74579dfc1424786be2748bb960a33a63d1c02214
Ignacio Rubio <ignaciorubio3@gmail.com>
2022-08-21 20:32:50 +0200

    Allow users to delete stock from being tracked

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,5 +9,17 @@ class UserStocksController < ApplicationController
 
     redirect_to my_portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.find_by(user_id: current_user, stock_id: stock)
+    if user_stock.destroy
+      flash[:notice] = "#{stock.ticker} was successfully removed from your portfolio"
+      redirect_to my_portfolio_path
+    else
+      flash[:error] = "Something went wrong trying to remove #{stock.ticker} from your portfolio"
+      redirect_to my_portfolio_path
+    end
+  end
   
 end
