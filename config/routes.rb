b5b7895f7af1d46ae82af10e7c019e3273ff328f
Ignacio Rubio <ignaciorubio3@gmail.com>
2022-08-22 22:22:12 +0200

    List friend from users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :user_stocks, only: [:create, :destroy]
-
-  devise_for :users
-
   root 'welcome#index'
-
-  get 'my_portfolio', to: 'users#my_portfolio'
   
+  # user
+  devise_for :users
+  
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
+  
+  # stocks
   get 'search_stock', to: 'stocks#search'
+
+  # user + stock relantion
+  resources :user_stocks, only: [:create, :destroy]
 end
