56ad65d90f5e625dd6013b960f94a1f056342b4c
Ignacio Rubio <ignaciorubio3@gmail.com>
2022-09-01 22:31:00 +0200

    Show users's friend list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,26 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friends
   end
+
+  def search_friend
+    respond_to do |format|
+      format.js {
+        if !params[:friend].present?
+          flash.now[:alert] = "Please enter a friend name or email to search"
+          render partial: 'users/friend_result' and return
+        end
+  
+        @friends = User.search(params[:friend])
+        @friends = current_user.except_current_user(@friends)
+  
+        unless @friends.present?
+          flash.now[:alert] = "Couldn't find user"
+          render partial: 'users/friend_result' and return
+        end
+        
+        render partial: 'users/friend_result'
+      }
+    end
+  end
   
 end
