6278984345afb629a10aa8c1c5c47504a3133efd
Ignacio Rubio <ignaciorubio3@gmail.com>
2022-09-05 21:59:26 +0200

    Allow following friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,18 +2,25 @@ class FriendshipsController < ApplicationController
   before_action :set_friendship, only: [:destroy]
   
   def create
+    current_user.friendships.build(friend_id: params[:friend_id])
+
+    if current_user.save
+      flash[:notice] = "Following friend"
+    else
+      flash[:alert] = "Cannot follow friend"
+    end
+
+    redirect_to my_friends_path
   end
 
   def destroy
     if @friendship.present? && @friendship.destroy
       flash[:notice] = "Stopped following"
-
-      redirect_to my_friends_path
     else
-      flash[:error] = "Cannot stop following"
-
-      redirect_to my_friends_path
+      flash[:alert] = "Cannot stop following"
     end
+
+    redirect_to my_friends_path
   end
 
   private
